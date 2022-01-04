84196a922768862a529f8bdd172d3be8478af466
Eray Bartan <ebartan@gmail.com>
2022-01-04 16:09:23 +0300

    logo

--- src/views/Home/Header.tsx
+++ src/views/Home/Header.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import LagrangeLogo from '../../../public/lagrangelogo.svg'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import React from 'react'
 
@@ -5,12 +7,13 @@ const Header = () => {
     return (
         <div className="navbar w-full mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
             <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <span className="text-4xl">🦤</span>
+                <button className="btn btn-square btn-ghost relative h-12 w-32">
+                    
+                    <Image src={LagrangeLogo} alt="Lagrange Logo" width="127" height="35"/>
                 </button>
             </div>
             <div className="flex-1 px-2 mx-2">
-                <span className="text-lg font-bold">Caw Caw</span>
+                
             </div>
             <div className="flex-none">
                 <WalletMultiButton className="btn btn-ghost" />
