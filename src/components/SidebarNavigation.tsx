e6a382b46f75076c520824368070b73cb12f93e8
Eray Bartan <ebartan@gmail.com>
2022-01-07 14:43:04 +0300

    navigation added Nextjs Link

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -1,19 +1,40 @@
-import Link from 'next/link'
+import Link from "next/link";
 import type { NextPage } from "next";
 const SidebarNavigation: NextPage = (props) => {
-    return(
-        <nav>
+  return (
+    <nav>
+      <Link href="/swap">
+        <a className="block py-3 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">
+          Swap
+        </a>
+      </Link>
+      <Link href="/market">
+        <a className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">
+          Market Overview
+        </a>
+      </Link>
+      <Link href="/account">
+        <a className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">
+          Account
+        </a>
+      </Link>
+      <Link href="/roadmap">
+        <a className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">
+          Roadmap
+        </a>
+      </Link>
+      <Link href="/whitepaper">
+        <a className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">
+          Whitepaper
+        </a>
+      </Link>
 
-       <Link href="/swap">
-         <a className="block py-3 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">Swap</a>
-       </Link>
-        <a href="/market" className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">Market Overview</a>
-        <a href="/account" className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">Account</a>
-        <a href="/roadmap" className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">Roadmap</a>
-        <a href="/whitepaper" className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">Whitepaper</a>
-        <a href="/settings" className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">Settings</a>
-     
-      </nav>
-    )
-}
-export default SidebarNavigation
+      <Link href="/settings">
+        <a className="block py-2.5 px-4 rounded-lg transition duration-200 hover:bg-gray-600 hover:text-white">
+          Settings
+        </a>
+      </Link>
+    </nav>
+  );
+};
+export default SidebarNavigation;
