84e4430d2cf5e9f9e891d1904ff3b5cb60a54427
ERAY BARTAN <eray.bartan@bodrumbld.local>
2022-01-31 17:05:03 +0300

    pools page added table not dynamic

--- src/pages/pools.tsx
+++ src/pages/pools.tsx
@@ -7,6 +7,11 @@ import { getData } from "./api/jupiter-api-example";
 import type { NextPage } from "next";
 import Image from "next/image";
 import { type } from "os";
+import EURS from "../../public/coin/2989.png";
+import JPYC from "../../public/coin/9045.png";
+import TRYB from "../../public/coin/5181.png";
+import BRZ from "../../public/coin/4139.png";
+
 const currencies = [
   {
     fiatSymbol: "USD",
@@ -50,11 +55,14 @@ const currencies = [
   },
 ];
 interface Props {
+  data: any;
+}
+interface Props {
   swappableOutputForSol: any;
 }
 const Pools: NextPage<Props> = (props) => {
   const [isExpanded, toggleExpansion] = useState(true);
-
+  const { data } = props;
   const { swappableOutputForSol } = props;
   console.log(swappableOutputForSol);
 
@@ -97,36 +105,161 @@ const Pools: NextPage<Props> = (props) => {
         <div className="py-2 text-center ">
           <h1 className="text-2xl">Pools</h1>
           {/* <h2 className="text-xl">Pools are in test mode please do not deposit</h2> */}
-          <div className="flex flex-wrap justify-between mx-1 overflow-hidden">
+          <div className="flex flex-wrap justify-between p-2 mx-1 overflow-hidden">
             <div className="w-1/4 py-2 mx-1 my-1 overflow-hidden bg-white border shadow-lg border-lagrangegraybackground rounded-xl">
-              <div className="py-4">Total Value Locked</div>
+              <div className="py-4 text-lg">Total Value Locked</div>
               <div>$7.487.195,25</div>
             </div>
 
             <div className="w-1/4 px-1 py-2 mx-1 my-1 overflow-hidden bg-white border shadow-lg border-lagrangegraybackground rounded-xl">
-              <div className="py-4">DFX Price Value</div>
+              <div className="py-4 text-lg">DFX Price Value</div>
               <div>$1,03</div>
             </div>
 
             <div className="w-1/4 px-1 py-2 mx-1 my-1 overflow-hidden bg-white border shadow-lg border-lagrangegraybackground rounded-xl">
-              <div className="py-4">My Total Value</div>
+              <div className="py-4 text-lg">My Total Value</div>
               <div>$-</div>
             </div>
           </div>
         </div>
-        <div>
-          <div className="px-2 py-2"></div>
+        <h1 className="text-lg text-center">Account Balance</h1>
+        <div className="flex justify-center p-2">
+          <div className="flex justify-center w-10/12 px-2 py-2 mx-1 my-1 overflow-hidden bg-white border shadow-lg border-lagrangegraybackground rounded-xl">
+            <table className="text-lg table-auto">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Price</th>
+                  <th>Balances</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs">
+                      <Image
+                        src="https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png"
+                        alt="USDC"
+                        width={32}
+                        height={32}
+                        layout="fixed"
+                      />
+                      <p className="px-2 py-2">USDC</p>
+                    </div>
+                  </td>
+                  <td>$ 1.000000</td>
+                  <td>0</td>
+                  <td>$ 0.000000</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs">
+                      <Image
+                        src={EURS}
+                        alt="EURS"
+                        width={32}
+                        height={32}
+                        layout="fixed"
+                      />
+                      <p className="px-2 py-2">EURS</p>
+                    </div>
+                  </td>
+                  <td>$ 1.000000</td>
+                  <td>0</td>
+                  <td>$ 0.000000</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs">
+                      <Image
+                        src={JPYC}
+                        alt="JPYC"
+                        width={32}
+                        height={32}
+                        layout="fixed"
+                      />
+                      <p className="px-2 py-2">JPYC</p>
+                    </div>
+                  </td>
+                  <td>$ 1.000000</td>
+                  <td>0</td>
+                  <td>$ 0.000000</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs">
+                      <Image
+                        src={TRYB}
+                        alt="TRYB"
+                        width={32}
+                        height={32}
+                        layout="fixed"
+                      />
+                      <p className="px-2 py-2">TRYB</p>
+                    </div>
+                  </td>
+                  <td>$ 1.000000</td>
+                  <td>0</td>
+                  <td>$ 0.000000</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs">
+                      <Image
+                        src={BRZ}
+                        alt="BRZ"
+                        width={32}
+                        height={32}
+                        layout="fixed"
+                      />
+                      <p className="px-2 py-2">BRZ</p>
+                    </div>
+                  </td>
+                  <td>$ 1.000000</td>
+                  <td>0</td>
+                  <td>$ 0.000000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 export default Pools;
+
 export async function getServerSideProps() {
-  const swappableOutputForSol = await getData();
+  const accessKey = "74676f0feb3ce4f81eda70c39b1eeaf9";
+  const endpoint = "https://api.currencylayer.com/live";
+  const sourceCurrencyPairs = currencies.map((source) => ({
+    ...source,
+    currencies: currencies
+      .map(({ fiatSymbol }) => fiatSymbol)
+      .filter((currency) => currency !== source.fiatSymbol),
+  }));
+
+  const pairs = await Promise.all(
+    sourceCurrencyPairs.map(async (pair) => {
+      const url = `${endpoint}?access_key=${accessKey}&source=${
+        pair.fiatSymbol
+      }&currencies=${pair.currencies.join(",")}&format=1`;
+      return {
+        ...pair,
+        quotes: (await (await fetch(url)).json()).quotes,
+      };
+    })
+  );
+
+  if (!pairs) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: {
-      swappableOutputForSol: swappableOutputForSol,
+      data: pairs,
     },
     // will be passed to the page component as props
   };
