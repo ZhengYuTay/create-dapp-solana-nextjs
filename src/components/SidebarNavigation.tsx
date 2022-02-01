d2218c03bb012a27640bf96f5338174d3c853dd3
ERAY BARTAN <eray.bartan@bodrumbld.local>
2022-02-01 16:34:05 +0300

    nav menu active state

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
-
+import { useRouter } from "next/router";
 import type { NextPage } from "next";
 import graphicReports from "../../public/graphic-reports.png";
 import swapIcon from "../../public/swap-icon.png";
 import poolIcon from "../../public/poolicon_black.png";
 const SidebarNavigation: NextPage = (props) => {
+  const router = useRouter();
   return (
     <nav>
       <Link href="/swap">
-        <a className="block px-2 py-3 transition duration-200 rounded-lg hover:bg-gray-600 hover:text-white">
+        <a className="block px-2 py-3 transition duration-200 rounded-lg hover:bg-gray-600 hover:text-white focus:outline-none focus:ring focus:ring-violet-300">
           <div className="flex">
             <div className="w-8 item">
               <Image
@@ -26,7 +27,14 @@ const SidebarNavigation: NextPage = (props) => {
         </a>
       </Link>
       <Link href="/overview">
-        <a className="block px-2 py-3 transition duration-200 rounded-lg hover:bg-gray-600 hover:text-white">
+        <a
+          className={
+            router.pathname == "/overview"
+              ? "block px-2 py-3 transition duration-200 rounded-lg bg-gray-600 text-white focus:outline-none focus:ring focus:ring-violet-300"
+              : "block px-2 py-3 transition duration-200 rounded-lg hover:bg-gray-600 hover:text-white"
+          }
+        >
+          {/* <a className="block px-2 py-3 transition duration-200 rounded-lg hover:bg-gray-600 hover:text-white"> */}
           <div className="flex">
             <div className="w-8 item">
               <Image
