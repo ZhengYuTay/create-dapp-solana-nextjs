eea2905a1242bca5ec646eee3874216b271d1c8e
Eray Bartan <ebartan@gmail.com>
2022-02-04 21:20:22 +0300

    select coin list with size

--- src/components/ChartContent.tsx
+++ src/components/ChartContent.tsx
@@ -34,9 +34,9 @@ const ChartContent: NextPage<Props> = (props) => {
     <div className="p-2 mx-2 mt-4 bg-white shadow-lg border-lagrangeborder w-112">
       <div>
         <div>
-          <p>USDC / USDT</p>
-          <p>1.00 - 0.00 %</p>
-          <p className="text-base">
+          <p className="font-normal">USDT / USDC</p>
+{/*           <p className="font-normal">1.00 - 0.00 %</p> */}
+          <p className="font-normal">
             {" "}
             {new Date().getDate()}
             {"/"}
@@ -57,7 +57,7 @@ const ChartContent: NextPage<Props> = (props) => {
         </div>
         <div className="flex items-center justify-between p-2 rounded bg-lagrangegraybackground">
           {" "}
-          <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs">
+          <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs font-normal">
             <Image
               src="https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png"
               alt="USDC"
@@ -67,9 +67,9 @@ const ChartContent: NextPage<Props> = (props) => {
             />
             <p className="px-2 py-2 text-white">USDT</p>
           </div>
-          <div className="flex items-center space-x-2">
-            <p className="pr-2 text-white">1.00$</p>{" "}
-            <p className="pr-2 text-lagrangered">0.00</p>
+          <div className="flex items-center space-x-2 font-normal">
+            <p className="pr-2 text-white font-normal">1.00$</p>{" "}
+            <p className="pr-2 text-lagrangered font-normal">0.00</p>
             <Image
               src={SelectDownIcon}
               alt="Select Down"
@@ -80,7 +80,7 @@ const ChartContent: NextPage<Props> = (props) => {
         <div className="h-4"></div>
         <div className="flex items-center justify-between p-2 rounded bg-lagrangegraybackground">
           {" "}
-          <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs">
+          <div className="flex flex-wrap items-center self-center justify-center sm:text-2xl xs:text-xs font-normal">
             <Image
               src="https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png"
               alt="USDC"
@@ -88,11 +88,11 @@ const ChartContent: NextPage<Props> = (props) => {
               height={32}
               layout="fixed"
             />
-            <p className="px-2 py-2 text-white">USDC</p>
+            <p className="px-2 py-2 text-white font-normal">USDC</p>
           </div>
           <div className="flex items-center space-x-2">
-            <p className="pr-2 text-white">1.00$</p>{" "}
-            <p className="pr-2 text-green-700">0.00</p>
+            <p className="pr-2 text-white font-normal">1.00$</p>{" "}
+            <p className="pr-2 text-green-700 font-normal">0.00</p>
             <Image
               src={SelectDownIcon}
               alt="Select Down"
@@ -117,36 +117,5 @@ export async function getServerSideProps() {
     },
   };
 
-  /* const accessKey = "74676f0feb3ce4f81eda70c39b1eeaf9";
-    const endpoint = "https://api.currencylayer.com/live";
-    const sourceCurrencyPairs = currencies.map((source) => ({
-      ...source,
-      currencies: currencies
-        .map(({ fiatSymbol }) => fiatSymbol)
-        .filter((currency) => currency !== source.fiatSymbol),
-    }));
   
-    const pairs = await Promise.all(
-      sourceCurrencyPairs.map(async (pair) => {
-        const url = `${endpoint}?access_key=${accessKey}&source=${
-          pair.fiatSymbol
-        }&currencies=${pair.currencies.join(",")}&format=1`;
-        return {
-          ...pair,
-          quotes: (await (await fetch(url)).json()).quotes,
-        };
-      })
-    );
-  
-    if (!pairs) {
-      return {
-        notFound: true,
-      };
-    }
-    return {
-      props: {
-        data: pairs,
-      },
-      // will be passed to the page component as props
-    }; */
 }
