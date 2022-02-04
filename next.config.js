0c018038307d6ce022912aa15f36fd6cdc4fd4cb
Eray Bartan <ebartan@gmail.com>
2022-02-04 16:16:12 +0300

    svgr webpack

--- next.config.js
+++ next.config.js
@@ -49,7 +49,21 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
-      
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              icon: true,
+  
+            },
+          },
+        ],
+  
+      });
     }
     
     return config;
