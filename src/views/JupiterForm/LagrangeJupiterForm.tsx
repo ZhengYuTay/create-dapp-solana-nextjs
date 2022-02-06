7d90711f7fed730c7e29baea3aa7f608fb9ec78b
Eray Bartan <ebartan@gmail.com>
2022-02-06 10:58:27 +0300

    swap button size full

--- src/views/JupiterForm/LagrangeJupiterForm.tsx
+++ src/views/JupiterForm/LagrangeJupiterForm.tsx
@@ -493,7 +493,7 @@ const LagrangeJupiterForm: FunctionComponent<IJupiterFormProps> = (props) => {
                   }
                 }
               }}
-              className="inline-flex justify-center h-11 px-2 py-2 text-base font-normal text-white uppercase rounded shadow-sm xs:w-full sm:w-10/12 place-items-center border-lagrangegraybackground bg-lagrangeswapbuttoncolor hover:bg-lagrangeswapbutton focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="inline-flex justify-center h-11 px-2 py-2 text-base font-normal text-white uppercase rounded shadow-sm xs:w-full place-items-center border-lagrangegraybackground bg-lagrangeswapbuttoncolor hover:bg-lagrangeswapbutton focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               <p className="text-xl text-center">Swap</p>
             </button>
@@ -501,7 +501,7 @@ const LagrangeJupiterForm: FunctionComponent<IJupiterFormProps> = (props) => {
             <button
               className={`${
                 loading ? "opacity-50 cursor-not-allowed" : ""
-              } xs:w-full sm:w-10/12 uppercase inline-flex justify-center place-self-end h-11 px-2 pt-2 border-lagrangebuttongray text-xl font-normal rounded shadow-sm text-lagrangebuttongray bg-gray-200 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 border`}
+              } xs:w-full  uppercase inline-flex justify-center place-self-end h-11 px-2 pt-2 border-lagrangebuttongray text-xl font-normal rounded shadow-sm text-lagrangebuttongray bg-gray-200 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 border`}
               type="button"
               onClick={refresh}
               disabled={loading}
