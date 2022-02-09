c9be8f8686e4ce38df04510d5eb9cbf00b4c986c
ERAY BARTAN <eray.bartan@bodrumbld.local>
2022-02-09 15:10:33 +0300

    swap margin top fixed

--- src/components/SwapContent.tsx
+++ src/components/SwapContent.tsx
@@ -5,27 +5,23 @@ import LagrangeJupiterForm from "../views/JupiterForm/LagrangeJupiterForm";
 const SwapContent: NextPage = (props) => {
   const { publicKey } = useWallet();
   return (
-    <div className="px-2 py-4 xs:px-2 xs:py-4">
- 
+    <div className="">
       <div className="flex flex-wrap justify-center w-full">
-
-      
-      {!publicKey ? <></> : <>
-      <div className="relative py-2 h-14 w-14 lg:mx-auto border-purple-500 rounded-full">
-          <Avatar/>
-          
+        {!publicKey ? (
+          <></>
+        ) : (
+          <>
+            <div className="relative py-2 border-purple-500 rounded-full h-14 w-14 lg:mx-auto">
+              <Avatar />
+            </div>
+            <div className="flex flex-wrap">
+              <span className="px-2 py-2 text-sm">Wallet Address</span>
+              <span className="px-2 py-2 text-2xs">{publicKey.toBase58()}</span>
+            </div>
+          </>
+        )}
       </div>
-     <div className="flex flex-wrap">
-     <span className="text-sm px-2 py-2">Wallet Address</span>
-     <span className="text-2xs px-2 py-2">{publicKey.toBase58()}</span>
-     </div>
-    
-    </>
-      } 
-          
-          
-          </div>
-          <LagrangeJupiterForm/>
+      <LagrangeJupiterForm />
     </div>
   );
 };
