03a38bab4479d8ef42a8dbce3f6ce947b16bc6a7
Eray Bartan <ebartan@gmail.com>
2022-02-09 20:36:59 +0300

    _document.js change content meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <meta
             name="description"
-            content="This site will fly high 🦤"
+            content="Lagrange.fi "
           />
           <link rel="icon" href="/favicon.ico" />
 
