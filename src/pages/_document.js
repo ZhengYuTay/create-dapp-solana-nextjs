c385dfc160caa58476ae9327a179666d6503618b
Eray Bartan <ebartan@gmail.com>
2022-02-10 23:19:06 +0300

    font weight lagrangewight

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,6 +29,9 @@ class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet"></link>
           {/* <meta name="theme-color" content="#ffffff" /> */}
 
           {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
