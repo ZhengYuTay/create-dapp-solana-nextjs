ae78aa5353eccb39977e49004a1a2b854ffd031b
Eray Bartan <ebartan@gmail.com>
2022-02-10 21:52:40 +0300

    font size theme config theme all size

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,15 @@ module.exports = {
     },
     fontSize: {
 
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
       'lagrange': '2.6875rem',
       'lagrangesmallscreen': '2.2675rem',
     },
