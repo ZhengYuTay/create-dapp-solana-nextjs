15adb737b3c32a1ea838ba9e4d2351f89a3c2519
ERAY BARTAN <eray.bartan@bodrumbld.local>
2022-02-10 10:07:58 +0300

    avatar

--- src/components/SwapContent.tsx
+++ src/components/SwapContent.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { useWallet } from "@solana/wallet-adapter-react";
-import Avatar from "./Avatar";
+/* import Avatar from "./Avatar"; */
 import LagrangeJupiterForm from "../views/JupiterForm/LagrangeJupiterForm";
 const SwapContent: NextPage = (props) => {
   const { publicKey } = useWallet();
@@ -12,11 +12,11 @@ const SwapContent: NextPage = (props) => {
         ) : (
           <>
             <div className="relative py-2 border-purple-500 rounded-full h-14 w-14 lg:mx-auto">
-              <Avatar />
+              {/*  <Avatar /> */}
             </div>
             <div className="flex flex-wrap">
-              <span className="px-2 py-2 text-sm">Wallet Address</span>
-              <span className="px-2 py-2 text-2xs">{publicKey.toBase58()}</span>
+              {/*  <span className="px-2 py-2 text-sm">Wallet Address</span>
+              <span className="px-2 py-2 text-2xs">{publicKey.toBase58()}</span> */}
             </div>
           </>
         )}
