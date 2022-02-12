3890accc92d9fdc32e053f95a9ad38c9ad87f596
Eray Bartan <ebartan@gmail.com>
2022-02-12 10:20:21 +0300

    chart js

--- src/pages/api/solana.tsx
+++ src/pages/api/solana.tsx
@@ -3,10 +3,11 @@ import https from "https";
 import cheerio from "cheerio";
 
 type User = {
-  id: number;
-  firstName: string;
-  lastName: string;
-  username: string;
+  date: string;
+/*   close: string;
+  marketcap: string;
+  volume: string; */
+
 };
 
 const getHtml = async (hostname: string, path: string): Promise<string> =>
@@ -37,48 +38,51 @@ const getHtml = async (hostname: string, path: string): Promise<string> =>
 const getTables = (html: string): cheerio.Cheerio => {
   const $ = cheerio.load(html);
   const tableElements = $(
-    "html body div.wrapper div.container table.table.table-bordered"
+    /* "html body div.wrapper div.container table.table.table-bordered" */
+    /* "html body div div[1] div[1] div[2] div div[3] div div div[2] table" */
+   "html body.DAY div#__next div.bywovg-1.fUzJes div.main-content div.sc-57oli2-0.comDeo.cmc-body-wrapper div.grid.full-width-layout div.sc-16r8icm-0.jKrmxw.container div.b4d71h-0.iTFIva div.b4d71h-2.hgYnhQ table.h7vnx2-2.hLKazY.cmc-table"
   );
+
+ 
   return tableElements;
 };
 
 const takeFirstTwoTables = (tables: cheerio.Cheerio): cheerio.Cheerio =>
   tables.slice(0, 2);
-
-
-
-
-
-
-
-
  
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
     const getUsers = (table: cheerio.Element): User[] => {
         const coins: User[] = [];
-      
+   
         const $ = cheerio.load(table);
-        $("tbody tr").each((_, row) => {
+        
+        $("thead tr").each((_, row) => {
+            
             coins.push({
-            id: Number($($(row).children()[0]).text()),
-            firstName: $($(row).children()[1]).text(),
-            lastName: $($(row).children()[2]).text(),
-            username: $($(row).children()[3]).text(),
-          });
+                date: $($(row).children()[1]).text(),
+               /*  close: $($(row).children()[2]).text(),
+                marketcap: $($(row).children()[3]).text(),
+                volume: $($(row).children()[4]).text(), */
+
+            });
         });
-      
+
+        
+        console.log($);
+     /*    
+       */
         return coins;
       };
-    getHtml("webscraper.io", "/test-sites/tables")
+    getHtml("coinmarketcap.com", "/currencies/tether/historical-data")
   .then(getTables)
   .then(takeFirstTwoTables)
-  .then((tables) => {
+/*   .then((tables) => {
     let coins: User[] = [];
     tables.each((_, table) => (coins = coins.concat(getUsers(table))));
     return coins;
-  })
-  .then((coins) => res.status(200).json({ coins }))
+  }) */
+  .then((getHtml) => console.log(getHtml))
   .catch((error) => console.log(error));
   
 }
